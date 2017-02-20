e6e26c568194e875a81ceed83cb5ed007bb1c759
BendersBender <ev.l.waits@gmail.com>
2017-02-20 15:57:26 -0800

    got tasks to save in local storage

--- content.js
+++ content.js
@@ -1,12 +1,33 @@
-// fetch("https://unsplash.it/200/300/?random").then(res => {
-//   console.log(res.status);
-//   console.log(res);
-// }).catch(err => {
-//   console.log(err);
-// })
+var addTaskButton = document.getElementById("add-task-button");
+var addTaskField = document.getElementById("new-task-field");
 
-(function(){
-  // var url = "https://unsplash.it/g/700/?random"
-  var body = document.getElementById("body");
-  body.style.backgroundImage= 'url("https://unsplash.it/g/400/?random")'
-})()
+
+addTaskButton.addEventListener("click", e => {
+  e.preventDefault()
+  var newTask = addTaskField.value;
+  saveNewTask(newTask);
+});
+
+function saveNewTask(task) {
+  var success = null;
+  chrome.storage.local.get("taskList", store => {
+
+    if(store.taskList === undefined || !Array.isArray(store.taskList)){
+      var toSave = store.taskList = [task];
+    } else {
+      var toSave = store.taskList
+      toSave.push(task)
+    }
+
+    chrome.storage.local.set({"taskList": toSave});
+  });
+
+  console.log("success flag:" + success);
+
+  if (success === true) {
+    return true;
+  } else {
+    return false;
+  }
+
+}

00b959ca701c454c6efccbdd2b602be9413e0bb0
BendersBender <ev.l.waits@gmail.com>
2017-02-19 17:31:49 -0800

    script linked

--- content.js
+++ content.js
@@ -1,6 +1,37 @@
+console.log("upper fuck");
 
-chrome.tabs.onCreated.addListener(tab => {
-  console.log(tab);
-  console.log("heyo");
-  alert("fuck");
-});
+// chrome.tabs.onCreated.addListener(tab => {
+//   var options = {
+//     methods: "GET"
+//   }
+//
+//   console.log("fuckkkk");
+//
+//   fetch('https://jsonplaceholder.typicode.com').then(
+//     function(response) {
+//       var body = document.getElementById('body');
+//
+//       if(response.status !== 200) {
+//         body.setInnerHTML = "There was a problem, status code: "+ response.status;
+//       } else {
+//         response.json().then(data => {
+//           console.log(data);
+//         });
+//       }
+//     }
+//   )
+// });
+
+fetch('https://jsonplaceholder.typicode.com').then(
+    function(response) {
+      var body = document.getElementById('body');
+
+      if(response.status !== 200) {
+        body.setInnerHTML = "There was a problem, status code: "+ response.status;
+      } else {
+        response.json().then(data => {
+          console.log(data);
+      });
+    }
+  }
+)

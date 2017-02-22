eedce7a12ff8de432e0b8bef5b67ec9f2f4b8651
BendersBender <ev.l.waits@gmail.com>
2017-02-21 16:10:19 -0800

    delete item from local storage on click

--- content.js
+++ content.js
@@ -5,6 +5,7 @@ var taskList = document.getElementById("task-list");
 
 
 
+
 addTaskButton.addEventListener("click", e => {
   e.preventDefault()
   var newTask = addTaskField.value;
@@ -16,13 +17,46 @@ function addTaskToUl(task){
   var newLi = document.createElement("li");
   var content = document.createTextNode(task);
   newLi.appendChild(content);
+
+  newLi.addEventListener("click", e => {
+    e.preventDefault();
+
+    toggleDelete(newLi)
+  });
+
   taskList.appendChild(newLi);
+}
+
+function toggleDelete(li){
+  var classes = li.classList;
+  var task = li.textContent
+
+  if (classes.contains("deleted")) {
+    classes.remove("deleted");
+    saveNewTask(task);
+  } else{
+    classes.add("deleted")
+    removeFromStorage(task);
+  }
+}
+
+
+function removeFromStorage(task) {
+  chrome.storage.local.get("taskList", store => {
+    var taskList = store.taskList;
+    var idxToDel = taskList.indexOf(task);
+
+    var left = taskList.slice(0, idxToDel);
+    var right = taskList.slice(idxToDel + 1);
+
+    var toSave = left.concat(right);
+
+    chrome.storage.local.set({taskList: toSave});
+  });
 }
 
 function saveNewTask(task) {
   chrome.storage.local.get("taskList", store => {
-
-    console.log(store);
 
     if(store.taskList === undefined || !Array.isArray(store.taskList)){
       var toSave = store.taskList = [task];
@@ -34,6 +68,11 @@ function saveNewTask(task) {
     chrome.storage.local.set({"taskList": toSave});
   });
 }
+
+
+
+
+
 
 function initialTaskAdd(){
   chrome.storage.local.get("taskList", store => {

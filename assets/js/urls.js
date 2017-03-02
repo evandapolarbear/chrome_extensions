8b9e9c4496170323f0a5fb0f3812816f9728a27b
BendersBender <ev.l.waits@gmail.com>
2017-03-02 13:56:16 -0800

    start

---
+++ assets/js/urls.js
@@ -0,0 +1,64 @@
+var urlInput = document.getElementById("url-input");
+var urlSubmit = document.getElementById("url-submit");
+var urlUl = document.getElementById("url-ul");
+
+urlSubmit.addEventListener("click", e = {
+  e.preventDefault()
+  aggUrlSave();
+});
+
+function aggUrlSave() {
+  var newUrl = urlInput.value;
+
+  if (newUrl === ''){
+    return
+  }
+
+  urlInput.value = '';
+  saveNewUrl(newUrl);
+  addUrlToUl(newUrl);
+}
+
+function saveNewUrl(url){
+  chrome.storage.local.get("urls", store => {
+    if(store.url === undefined || !Array.isArray(store.urls)){
+      var toSave = store.urls = [url];
+    } else {
+      var toSave = store.urls
+      toSave.push(url)
+    }
+
+    chrome.storage.local.set({"urls" :toSave});
+  });
+}
+
+function addUrlToUl(url){
+  var newLi = document.createElement("li");
+  newLi.classList.add("url-li");
+
+  var inner = document.createElement("a");
+  inner.setAttribute("href", url);
+  inner.classList.add("url-a");
+
+  var content = document.createTextNode(url)
+  inner.appendChild(content);
+
+  newLi.appendChild(inner);
+}
+
+function initialUrlAdd() {
+  console.log("fuckkkkkkkk");
+  chrome.storage.local.get("urls", store => {
+    var urls = store.urls;
+
+    if (urls.length > 0 ){
+      for (let i = 0; i urls.length; i++){
+        addUrlToUl(urls[i])
+      }
+    } else {
+      urlUl.classList.add("hidden");
+    }
+  })
+}
+
+initialUrlAdd();

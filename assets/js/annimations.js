77f310f99fc1ce92c00ab21c3885cd7a445fa262
BendersBender <ev.l.waits@gmail.com>
2017-03-07 15:22:37 -0800

    basic see more animation

--- assets/js/annimations.js
+++ assets/js/annimations.js
@@ -1,30 +1,39 @@
 var moreCircleOne = document.getElementById("more-circle-1")
 var moreCircleTwo = document.getElementById("more-circle-2")
 var moreCircleThree = document.getElementById("more-circle-3")
+var moreCircleFour = document.getElementById("more-circle-4")
 
 setInterval(() => {
-  moreCircleOne.classList.remove("more-circle");
   moreCircleOne.classList.add("active-more-circle");
+  moreCircleOne.classList.remove("more-circle");
 
   setTimeout(() => {
-    moreCircleOne.classList.remove("active-more-circle");
     moreCircleOne.classList.add("more-circle");
+    moreCircleOne.classList.remove("active-more-circle");
 
-    moreCircleTwo.classList.remove("more-circle");
     moreCircleTwo.classList.add("active-more-circle");
+    moreCircleTwo.classList.remove("more-circle");
   }, 150);
 
   setTimeout(() => {
-    moreCircleTwo.classList.remove("active-more-circle");
     moreCircleTwo.classList.add("more-circle");
+    moreCircleTwo.classList.remove("active-more-circle");
 
-    moreCircleThree.classList.remove("more-circle");
     moreCircleThree.classList.add("active-more-circle");
+    moreCircleThree.classList.remove("more-circle");
   }, 300);
 
   setTimeout(() => {
-    moreCircleThree.classList.remove("active-more-circle");
     moreCircleThree.classList.add("more-circle");
+    moreCircleThree.classList.remove("active-more-circle");
+
+    moreCircleFour.classList.add("active-more-circle")
+    moreCircleFour.classList.remove("more-circle")
   }, 450);
 
-}, 3500);
+  setTimeout(() => {
+    moreCircleFour.classList.add("more-circle");
+    moreCircleFour.classList.remove("active-more-circle");
+  }, 600);
+
+}, 4000);
